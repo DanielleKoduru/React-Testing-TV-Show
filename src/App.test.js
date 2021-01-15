fe29be827ff8e0ab03baa0c4da10351aa7ac0cdc
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-01-14 20:13:18 -0700

    met MVP, all tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { render, screen, userEvent } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import App from "./App";
-import { act } from "react-dom/test-utils";
 
-test("App component renders correctly", () => {
-    render(<App show={false} />);
-});
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
+import { testData } from "./data";
 
-test("App renders select a season button with data", async () => {
+jest.mock('./api/fetchShow');
+
+test('renders the data', async () => {
     mockFetchShow.mockResolvedValueOnce(testData);
-    const { rerender, getByText } = render(<App />);
+    render(<App />);
     screen.debug();
-    await act(async () => {
-        await rerender(<App />);
-        const dropDown = getByText(/select a season/i);
-        userEvent.click(dropDown);
-        screen.debug();
+    await waitFor(() => {
+      expect(screen.getByText(/fetching/i)).toBeInTheDocument();
     });
-};
+  });
+  
+  test('renders the sections from API call', async () => {
+    mockFetchShow.mockResolvedValueOnce(testData);
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByText(/a love letter/i)).toBeInTheDocument();
+    });
+  });
+  
+  test('renders the season list when dropdown is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(testData);
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByText(/a love letter/i)).toBeInTheDocument();
+    });
+    screen.debug();
+    userEvent.click(screen.getByText(/select a season/i));
+    userEvent.click(screen.getByText(/season 1/i));
+    await waitFor(() => {
+      expect(screen.getByText(/chapter one: the vanishing of will byers/i)).toBeInTheDocument();
+    });
+  });
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,10 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Episodes from "./Episodes";
 
-test("Episodes list shows data when a season is clicked", () => {
+test("renders without errors", () => {
+    render(<Episodes episodes={[]} />);
+});
+
+test("Episodes render correctly with info", () => {
     const { queryAllByTestId } = render(<Episodes episodes={[]} />);
     const seasonEpisodes = queryAllByTestId(/episode/i);
     expect(seasonEpisodes).toHaveLength(0);
     expect(seasonEpisodes).toEqual([]);
 });
+
 
